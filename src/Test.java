729a1b183e6530567cdabe985d1febbb9f53f6ab
miray <mirayayerdem@gmail.com>
2020-10-21 21:29:02 +0300

    Part f is completed

--- src/Test.java
+++ src/Test.java
@@ -1,18 +1,47 @@
 import cs1.SimpleURLReader;
-
-public class Main {
+/**
+ * a class that tests HTMLFilteredReader, MySimpleURLReader and WordSearch classes
+ * @author Miray Ayerdem
+ * @version 18/10/2020
+ */
+public class Test {
     public static void main(String[] args)
     {
-       //SimpleURLReader page = new SimpleURLReader("http://www.cs.bilkent.edu.tr/~ozturk/cs102/housman.txt");
-        SimpleURLReader page = new SimpleURLReader("https://scrapethissite.com/pages/simple");
-        System.out.println(page);
-
         int numberOfLine;
         String content;
+
+        SimpleURLReader page = new SimpleURLReader("http://www.cs.bilkent.edu.tr/~ozturk/cs102/housman.txt");
+        //PART A
         content = page.getPageContents();
         numberOfLine = page.getLineCount();
         System.out.println(content);
-        System.out.println("The number of lines is: " + numberOfLine);
+        System.out.println("The content " + numberOfLine + " lines!");
+        //PART B
+        MySimpleURLReader page2 = new MySimpleURLReader("https://scrapethissite.com/pages/simple");
+        System.out.println("The url of the page: " + page2.getURL());
+
+        //PART C
+        System.out.println("CSS Links on the website: ");
+        for(int i = 0 ; i < page2.getNumberOfCSSLinks().length ; i++)
+        {
+            System.out.println( page2.getNumberOfCSSLinks()[i]);
+        }
+        System.out.println("The number of the CSS Links on the website: " + page2.getNumberOfCSSLinks().length);
+
+        //PART D
+        HTMLFilteredReader page3 = new HTMLFilteredReader("https://scrapethissite.com/pages/simple/");
+        System.out.println(page3.getPageContents());
+
+        //PART E
+        WordSearch page4 = new WordSearch("https://scrapethissite.com/pages/simple/");
+        page4.checkWord("Turkey");
+        page4.checkWord("Ankara");
+        page4.checkWord("St. Johns");
+
+
+
+
+
 
     }
 }
